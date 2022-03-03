5f5d96dbe9a92c4586119706569f4932064f93ab
karibays <77321432+karibays@users.noreply.github.com>
2022-03-04 02:46:19 +0600

    test_2

--- Classification/settings.py
+++ Classification/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
-MEDIA_URL = "/media/"
+MEDIA_URL = "media"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
